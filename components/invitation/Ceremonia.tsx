e1f68de3fb875be2cc37d21068d9e5dcd624e168
Isaias Castillo <isaias.castillo@utp.ac.pa>
2026-08-01 13:49:09 -0500

    fix: update map link for Parroquia location in Ceremonia component

--- components/invitation/Ceremonia.tsx
+++ components/invitation/Ceremonia.tsx
@@ -51,7 +51,7 @@ export default function Intro() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ delay: 0.25, duration: 0.7 }}
-          href="https://www.google.com/maps/search/?api=1&query=Hotel+Contempo"
+          href="https://www.google.com/maps/place/Parroquia+Jes%C3%BAs+de+la+Divina+Misericordia-Arquidiocesis+de+Managua/@12.1030623,-86.305072,14z/data=!4m10!1m2!2m1!1s+Parroquia+Jes%C3%BAs+de+la+Divina+Misericordia!3m6!1s0x8f71559ab16a43bf:0x7e8d88b1f84aab49!8m2!3d12.1030623!4d-86.2669632!15sCipQYXJyb3F1aWEgSmVzw7pzIGRlIGxhIERpdmluYSBNaXNlcmljb3JkaWGSAQZjaHVyY2jgAQA!16s%2Fg%2F1tfryjw0?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D"
           target="_blank"
           rel="noreferrer"
           className="flex justify-center gap-2 py-3 mt-6 text-white transition rounded-lg justify-center1/12 bg-wedding-dark hover:bg-wedding-dark/90"
